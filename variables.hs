812e0d9aaf03793d79cd63ed91751844f62c991c
Nicolas Lopes <nicolaslopesbusiness@gmail.com>
2021-11-09 22:32:26 -0300

    learning about variables

---
+++ variables.hs
@@ -0,0 +1,8 @@
+main = do
+    let batata = "salve salve fml"
+
+    putStrLn batata
+    putStr ("eae" ++ batata)
+    -- ⬆️ concatenate needs brackets
+    putStr (show 36)
+    -- ⬆️ print numbers needs show function to convert the number to a string
